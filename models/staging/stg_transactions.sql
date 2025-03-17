8348d5e1ae012f2932c261b4e261418070787719
Suvetha S <131991850+Suvetha1111@users.noreply.github.com>
2025-03-17 12:01:16 +0000

    date issue fixed

--- models/staging/stg_transactions.sql
+++ models/staging/stg_transactions.sql
@@ -9,5 +9,5 @@ SELECT
     customer_id,
     product_id,
     CAST(COALESCE(revenue, 0) AS FLOAT) AS revenue,  -- Handle NULL revenue
-    TO_TIMESTAMP(RIGHT(payment_month, 4) || '-' || SUBSTR(payment_month, 4, 2), 'YYYY-MM') AS payment_month_clean  -- Convert VARCHAR to TIMESTAMP
+   TO_DATE(payment_month ,'DD-MM-YYYY') AS payment_month_clean, -- Convert VARCHAR to DATE
 FROM raw
